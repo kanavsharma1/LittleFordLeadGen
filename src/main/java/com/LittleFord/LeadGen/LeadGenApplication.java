36ad49097da302f6eef9c78362223ebb58c50bd4
kanav sharma <kanavsharma1@outlook.com>
2019-08-28 16:12:59 +0530

    first commit

---
+++ src/main/java/com/LittleFord/LeadGen/LeadGenApplication.java
@@ -0,0 +1,13 @@
+package com.LittleFord.LeadGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LittleFord/LeadGen/LeadGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LittleFord.LeadGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
